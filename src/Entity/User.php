b87f8d65a61c0298b55cbf4fa542e6b7ad9f6dc2
william <william.del.rosario@iceshop.nl>
2019-08-11 23:56:08 +0200

    collections-write

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -65,7 +65,7 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\CheeseListing", mappedBy="owner")
-     * @Groups({"user:read"})
+     * @Groups({"user:read", "user:write"})
      */
     private $cheeseListings;
 
